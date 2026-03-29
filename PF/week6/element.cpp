b4870c3474248f9c952db4c763357b939356c6f4
sameenshbr0807 <sameenshbr0807@gmail.com>
2026-03-29 07:31:42 -0700

    Delete PF directory

--- PF/week6/element.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-using namespace std;
-main() {
-    int number[5]={3,6,2,9,7};
-    cout<<"Element at 2nd index is:"<< number[1]<<endl;
-    cout<<"Element at 4th index is:"<< number[3]<<endl;
-
-}
--- PF/week6/something.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-using namespace std;
-main() {
-    char a[100];
-    cout<<"Enter the argument 'a':";
-     cin.getline(a,100);
-     cout<<"Results: something " << a <<endl;
-    }
