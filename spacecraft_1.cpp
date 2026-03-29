4ea330d0e6b9f1a11dd528a08f60f5b30840f14f
sameenshbr0807 <sameenshbr0807@gmail.com>
2026-03-29 07:07:29 -0700

    Delete spacecraft_1.cpp

--- spacecraft_1.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-using namespace std;
-main()  {   
-cout <<"         /\\              "<<endl;
-cout <<"       _/~~\\_             "<<endl;
-cout <<"     /  |  |  \\          "<<endl;
-cout <<"   =====.  .=====        "<<endl;
-cout <<"        ||||            "<<endl;
-}
